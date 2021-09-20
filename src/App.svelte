dcf90a4d0ebf78f324e04e4c54a6532079169358
Adam <adamrpostjr@gmail.com>
2021-09-20 18:30:44 -0400

    Building out all Buttons

--- src/App.svelte
+++ src/App.svelte
@@ -3,8 +3,47 @@
 </script>
 
 <main>
-  <Button sound="/anal_seepage.mp3">Anal Seepage</Button>
-  <Button sound="/blood_pressure_attack.mp3">Blood Pressure Attack</Button>
+  <Button sound="/static-sounds/anal_seepage.mp3">anal seepage</Button>
+  <Button sound="/static-sounds/blood_pressure_attack.mp3">blood pressure attack</Button>
+  <Button sound="/static-sounds/can_you_grow_up.mp3">can you grow up</Button>
+  <Button sound="/static-sounds/defecate_in_a_diaper.mp3">defecate in a diaper</Button>
+  <Button sound="/static-sounds/freak_the_freak_out.mp3">freak the freak out</Button>
+  <Button sound="/static-sounds/fun_is_not_the_word_id_use.mp3">fun is not the word id use</Button>
+  <Button sound="/static-sounds/hah_no.mp3hillbilly_haywood.mp3">hah nohillbilly haywood</Button>
+  <Button sound="/static-sounds/i_hate_this_office.mp3">i hate this office</Button>
+  <Button sound="/static-sounds/i_have_one_more_bite.mp3">i have one more bite</Button>
+  <Button sound="/static-sounds/i_need_someone_whos_not_incompetent.mp3">i need someone whos not incompetent</Button>
+  <Button sound="/static-sounds/idiot.mp3">idiot</Button>
+  <Button sound="/static-sounds/im_a_man.mp3">im a man</Button>
+  <Button sound="/static-sounds/im_gonna_f-ing_cut_your_eyes_out.mp3">im gonna f-ing cut your eyes out</Button>
+  <Button sound="/static-sounds/in_a_public_place.mp3">in a public place</Button>
+  <Button sound="/static-sounds/its_like_talking_to_an_absolute_idiot.mp3">its like talking to an absolute idiot</Button>
+  <Button sound="/static-sounds/jimminy_crickets.mp3">jimminy crickets</Button>
+  <Button sound="/static-sounds/light_you_on_fire.mp3">light you on fire</Button>
+  <Button sound="/static-sounds/my_gosh_what_is_wrong_with_you.mp3">my gosh what is wrong with you</Button>
+  <Button sound="/static-sounds/no_no_no_no_noooo.mp3">no no no no noooo</Button>
+  <Button sound="/static-sounds/not_important.mp3">not important</Button>
+  <Button sound="/static-sounds/phillip_schneedly.mp3">phillip schneedly</Button>
+  <Button sound="/static-sounds/punched_in_the_throat.mp3">punched in the throat</Button>
+  <Button sound="/static-sounds/purdy_mouth.mp3">purdy mouth</Button>
+  <Button sound="/static-sounds/ram_a_car.mp3">ram a car</Button>
+  <Button sound="/static-sounds/scrumdiddlyumptious.mp3">scrumdiddlyumptious</Button>
+  <Button sound="/static-sounds/shower_you.mp3">shower you</Button>
+  <Button sound="/static-sounds/tell_me_more_about_yourself.mp3">tell me more about yourself</Button>
+  <Button sound="/static-sounds/that_doesnt_make_sense.mp3">that doesnt make sense</Button>
+  <Button sound="/static-sounds/this_is_james.mp3">this is james</Button>
+  <Button sound="/static-sounds/transfer_me_now.mp3">transfer me now</Button>
+  <Button sound="/static-sounds/transfer_me_or_youre_fired.mp3">transfer me or youre fired</Button>
+  <Button sound="/static-sounds/wang.mp3">wang</Button>
+  <Button sound="/static-sounds/we_need_to_fire_adam.mp3">we need to fire adam</Button>
+  <Button sound="/static-sounds/what_is_wrong_with_you.mp3">what is wrong with you</Button>
+  <Button sound="/static-sounds/who_would_be_laughing.mp3">who would be laughing</Button>
+  <Button sound="/static-sounds/yeah.mp3">yeah</Button>
+  <Button sound="/static-sounds/you_fricken_hillbilly.mp3">you fricken hillbilly</Button>
+  <Button sound="/static-sounds/you_people_are_the_worst.mp3">you people are the worst</Button>
+  <Button sound="/static-sounds/you_smell_like_poo.mp3">you smell like poo</Button>
+  <Button sound="/static-sounds/youre_embarrassing_me_in_front_of_the_office.mp3">youre embarrassing me in front of the office</Button>
+  <Button sound="/static-sounds/youre_the_dumbest_person.mp3">youre the dumbest person</Button>
 </main>
 
 <style>
